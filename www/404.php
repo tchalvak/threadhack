9c2d85470f4222e7f171c44aabf1b2fc515358a6
Roy <tchalvakspam@gmail.com>
2010-03-24 11:13:18 -0400

    Initializing: 404 page and base lib include.

---
+++ www/404.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!-- html5 doctype enforces most compliant standards, even though use of html5 features isn't practical quite yet -->
+<html>
+  <head>
+    <title>
+      404 Error Page Not Found
+    </title>
+    <!--[if lte IE 6]></base><![endif]-->
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+  </head>
+  <body id="page-404">
+  	<h1>Page Not Found</h1>
+  	<img src='images/threadhackicon.png' alt=''>
+    <p>
+      Sorry, that page isn't there, feel free to search the site for a similar one, though.
+    </p>
+<form action="http://www.google.com/search" name="searchbox" method="get" style="margin-left: 2em;"> 
+  <input type="hidden" name="hl" value="en"> 
+  <input type="hidden" name="ie" value="ISO-8859-1"> 
+  <input type="hidden" name="sitesearch" value="royronalds.com"> 
+  <input maxlength="256" size="40" name="q" value=""> 
+  <input type="submit" value="search the ninjawars site" name="btnG" style="font-size:75%;"> 
+</form>
+    <div>Return to <a href="/">RoyRonalds.com</a></div>
+    <div id='support-email'>or email <a href="mailto:roy.ronalds@gmail.com">roy.ronalds@gmail.com</a></div>
+  </body>
+</html>
